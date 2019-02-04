e425b7b39c1c86eb2d1d6c4c2a514de913ad81d6
ichikawa-riku <owner@M15-108.local>
2019-02-04 21:44:53 +0900

    create massage sendding
    ・メッセージ送信機能追加
    ・左ナビ最新メッセージ取得機能追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,8 +7,8 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = @group.message.includes(:user)
-    if @message.sace
+    @message = @group.messages.new(message_params)
+    if @message.save
       redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
       @messages = @group.messages.includes(:user)
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'groups#index'
   devise_for :users
   resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:edit, :update, :new, :create] do resources :messages, only: [:index, :create] end
+  resources :groups, only: [:edit, :update, :new, :create] do
+      resources :messages, only: [:index, :create]
+    end
 end
