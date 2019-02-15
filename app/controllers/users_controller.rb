35e28d40796641a7e921b3b2fa2f059a55a11fe0
ichikawa-riku <owner@M15-108.local>
2019-02-15 21:05:03 +0900

    edit users#index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.where('name LIKE ?', "#{params[:keyword]}%")
+    respond_to do |function|
+      function.html
+      function.json
+    end
+  end
+
   def edit
   end
 
