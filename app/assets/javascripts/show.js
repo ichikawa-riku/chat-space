18fc7b34970222d4ecaf6359f650e2dae1ab02b0
ichikawa-riku <owner@M15-108.local>
2019-02-15 18:49:23 +0900

    edit show.js
    ・デバックを削除

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -20,7 +20,6 @@ $(function(){
   }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    console.log('イベントが発火されました')
     var formData = new FormData(this);
     var url = $(this).attr('action');
     $.ajax({
