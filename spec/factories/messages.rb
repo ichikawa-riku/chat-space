df8c6700ee93180c5b61bbb291459c69378f6aa7
ichikawa-riku <owner@M15-108.local>
2019-02-20 20:36:23 +0900

    add test.jpg

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :message do
     content Faker::Lorem.sentence
-    image File.open("#{Rails.root}/public/uploads/message/no_image.jpg")
+    image File.open("#{Rails.root}/public/no_image.jpg")
     user
     group
   end
