cbf26e417bb448923b8b622e2f46b4686a05c277
ichikawa-riku <owner@M15-108.local>
2019-02-21 19:28:16 +0900

    修正
    ・更新しないと非同期通信投稿できないバグを解消
    ・ログアウトできないバグを解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'groups#index'
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:edit, :update, :new, :create] do
       resources :messages, only: [:index, :create]
